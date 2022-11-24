0a7897cf3ed6d31e665be7a734476cd23f9560c5
nizhantiw <nizhantiw@gmail.com>
2022-11-24 09:59:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,23 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
+        const relativeList=[
+            {relativeListItem1: 'Sidhu Moose Wala', key: 0},
+            {relativeListItem2: 'Jon Bossmmana', key: 1},
+            {relativeListItem3: 'Diljit Dosanjh', key: 2}
+            ];
 
         return(
+            
             <div id="main">
-               {/* Do not remove the main div */}
+            <ol>
+               {/* Do not remove the main div */ relativeList.map(relative =>{
+            return (
+            <li key={relative.key}<{relative.relativeList}</li>
+);
+})}
+</ol>
+);
             </div>
         )
     }
